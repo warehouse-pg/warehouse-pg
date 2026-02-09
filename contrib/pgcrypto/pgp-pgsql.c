67b37ff734e564fce6c97ad675ead1ad23238141
Noah Misch <noah@leadboat.com>
2026-02-09 06:14:47 -0800

    Require PGP-decrypted text to pass encoding validation.
    pgp_sym_decrypt() and pgp_pub_decrypt() will raise such errors, while
    bytea variants will not.  The existing "dat3" test decrypted to non-UTF8
    text, so switch that query to bytea.
    
    The long-term intent is for type "text" to always be valid in the
    database encoding.  pgcrypto has long been known as a source of
    exceptions to that intent, but a report about exploiting invalid values
    of type "text" brought this module to the forefront.  This particular
    exception is straightforward to fix, with reasonable effect on user
    queries.  Back-patch to v14 (all supported versions).
    
    Reported-by: Paul Gerste (as part of zeroday.cloud)
    Reported-by: Moritz Sanft (as part of zeroday.cloud)
    Author: shihao zhong <zhong950419@gmail.com>
    Reviewed-by: cary huang <hcary328@gmail.com>
    Discussion: https://postgr.es/m/CAGRkXqRZyo0gLxPJqUsDqtWYBbgM14betsHiLRPj9mo2=z9VvA@mail.gmail.com
    Backpatch-through: 14
    Security: CVE-2026-2006
    (cherry picked from commit 8373ed09440f0f20582da3e4b95e6d2eb76e99bb)
    
    WarehousePG adaptation (upstream is PG14+, we are PG9.4-based):
    
    - contrib/pgcrypto/expected/pgp-decrypt_1.out does not exist on this branch
      (it is the alternative expected file upstream added for --without-zlib
      builds), so that hunk is dropped.  The follow-up commit that makes this
      test zlib-independent is applied next.

--- contrib/pgcrypto/pgp-pgsql.c
+++ contrib/pgcrypto/pgp-pgsql.c
@@ -649,6 +649,7 @@ pgp_sym_decrypt_text(PG_FUNCTION_ARGS)
 		arg = PG_GETARG_BYTEA_P(2);
 
 	res = decrypt_internal(0, 1, data, key, NULL, arg);
+	pg_verifymbstr(VARDATA_ANY(res), VARSIZE_ANY_EXHDR(res), false);
 
 	PG_FREE_IF_COPY(data, 0);
 	PG_FREE_IF_COPY(key, 1);
@@ -750,6 +751,7 @@ pgp_pub_decrypt_text(PG_FUNCTION_ARGS)
 		arg = PG_GETARG_BYTEA_P(3);
 
 	res = decrypt_internal(1, 1, data, key, psw, arg);
+	pg_verifymbstr(VARDATA_ANY(res), VARSIZE_ANY_EXHDR(res), false);
 
 	PG_FREE_IF_COPY(data, 0);
 	PG_FREE_IF_COPY(key, 1);
