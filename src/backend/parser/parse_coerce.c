18df90110d7d427e8383d177fac948965c93d7cb
Tom Lane <tgl@sss.pgh.pa.us>
2026-08-10 06:38:35 -0700

    Reject calls from SQL to functions that take or return type internal.
    Allowing that is a security hole, since there are many different
    functions with different ideas of what their "internal" argument or
    result is.  We already had a defense against the easy case of
    "'foo'::internal", but that turns out to be insufficient.  Lock down
    both function and operator syntax.  Also disallow attempts to cast to
    or from type internal; those would mostly fail anyway, but we have
    created some holes with features such as CoerceViaIO.
    
    Reported-by: Amy Burnett (OpenAI Codex Security)
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Reviewed-by: Robert Haas <robertmhaas@gmail.com>
    Backpatch-through: 14
    Security: CVE-2026-14680
    (cherry picked from commit 155dacbc5479a9bfbcb47e88ff68a758c73b6a8c)

--- src/backend/parser/parse_coerce.c
+++ src/backend/parser/parse_coerce.c
@@ -900,13 +900,17 @@ can_coerce_type(int nargs, Oid *input_typeids, Oid *target_typeids,
 		if (inputTypeId == targetTypeId)
 			continue;
 
-		/* 
-		 * ANYTABLE is a special case that can occur when a function is 
+		/* reject all cases of casting something else to/from "internal" */
+		if (inputTypeId == INTERNALOID || targetTypeId == INTERNALOID)
+			return false;
+
+		/*
+		 * ANYTABLE is a special case that can occur when a function is
 		 * called with a TableValue expression.  A table value expression
 		 * can only match a parameter to a function defined as a "anytable".
 		 *
 		 * Only allow ANYTABLE to match another ANYTABLE, anything else would
-		 * be a mismatch of Table domain and Value domain expressions.  
+		 * be a mismatch of Table domain and Value domain expressions.
 		 *
 		 * Validation of ANYTABLE coercion is processed at a higher level
 		 * that has more context related to the tupleDesc for the tables
@@ -2525,6 +2529,10 @@ find_coercion_pathway(Oid targetTypeId, Oid sourceTypeId,
 	/* Domains are always coercible to and from their base type */
 	if (sourceTypeId == targetTypeId)
 		return COERCION_PATH_RELABELTYPE;
+
+	/* Reject all cases of casting something else to/from "internal" */
+	if (sourceTypeId == INTERNALOID || targetTypeId == INTERNALOID)
+		return COERCION_PATH_NONE;
 
 	/* SELECT castcontext from pg_cast */
 
